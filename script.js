59a2b60dbdfc8d6f12b0f9607f3436e60ff4c70c
MateusBarbosaCode <mateusbarbosa4509@gmail.com>
2026-03-17 23:51:39 -0300

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const botao = document.getElementById('meuBotao');
-const icone = document.getElementById('iconeBotao');
-const temaSalvo = localStorage.getItem('tema');
+const botaoTema = document.getElementById('meuBotao');
+const iconeBotao = document.getElementById('iconeBotao');
 
-if (temaSalvo === 'claro') {
-    document.body.classList.add('dark-mode');
-    icone.src = "img/sol.png";
-}
-
-botao.addEventListener('click', () => {
+botaoTema.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-    const eClaro = document.body.classList.contains('dark-mode');
-    icone.src = eClaro ? "img/sol.png" : "img/lua.png";
-    localStorage.setItem('tema', eClaro ? 'claro' : 'escuro');
+    
+    // Troca o ícone se você tiver as duas imagens na pasta img
+    if (document.body.classList.contains('dark-mode')) {
+        iconeBotao.src = 'img/sol.png'; // Caso não tenha sol.png, ele apenas mudará a cor da lua
+    } else {
+        iconeBotao.src = 'img/lua.png';
+    }
 });
