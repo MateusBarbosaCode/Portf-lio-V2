3fb3ad07c2aebc1daf36c60b9a267b37c3e7c528
MateusBarbosaCode <mateusbarbosa4509@gmail.com>
2026-03-18 00:26:23 -0300

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 const botaoTema = document.getElementById('meuBotao');
 const iconeBotao = document.getElementById('iconeBotao');
 
-// Carregar preferência salva
 const temaSalvo = localStorage.getItem('tema');
 if (temaSalvo === 'claro') {
     document.body.classList.add('dark-mode');
-    iconeBotao.src = 'img/sol.png';
 }
 
 botaoTema.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-    const estaNoClaro = document.body.classList.contains('dark-mode');
-    
-    localStorage.setItem('tema', estaNoClaro ? 'claro' : 'escuro');
-    iconeBotao.src = estaNoClaro ? 'img/sol.png' : 'img/lua.png';
+    localStorage.setItem('tema', document.body.classList.contains('dark-mode') ? 'claro' : 'escuro');
 });
